c4f9bd66c3940e86c8e6e952173a1c9b33f3b113
foxvac <38643902+foxvac@users.noreply.github.com>
2018-04-30 12:42:48 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-cd /root/libpcap-1.8.1/PUBG-Radar-Onekey
-=======
-cd /root/libpcap-1.8.1/Shool
->>>>>>> 提交文件
+cd /root/libpcap-1.8.1/tries4/
 read -p "请输入内网ip： " ip
 forever start index.js sniff eth0 $ip | pino
 echo "启动完成"
